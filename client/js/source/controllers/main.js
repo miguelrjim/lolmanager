ca42918dd818a6d5ed828f94e85325eb0cf772ee
Miguel Jimenez <miguel.ricardo.jimenez.gomez@gmail.com>
2016-05-09 01:26:05 -0700

    saving users to file

--- client/js/source/controllers/main.js
+++ client/js/source/controllers/main.js
@@ -5,27 +5,37 @@ angular.module('LOLUserManager')
   .controller('Main', ['$scope', '$http',
     function($scope, $http) {
       $scope.regions = [
-        "br",
-        "eune",
-        "euw",
-        "jp",
-        "kr",
-        "lan",
-        "las",
-        "na",
-        "oce",
-        "ru",
-        "tr"
-      ]
+        'br',
+        'eune',
+        'euw',
+        'jp',
+        'kr',
+        'lan',
+        'las',
+        'na',
+        'oce',
+        'ru',
+        'tr'
+      ];
+      $scope.userToAdd = {
+        region: 'lan'
+      }
       $http.get('/users')
         .then(response => {
           $scope.users = response.data
         });
-      $scope.addUser = function() {
+      $scope.addUser = () => {
         $http.post('/users/add', $scope.userToAdd)
           .then(response => {
             $scope.users.push(response.data)
           })
+      }
+      $scope.currentGame = user => {
+        $http.get(`/users/${user.id}/currentGame`)
+          .then(response => {
+            user.inGame = response.data.inGame;
+            user.inGameTimestamp = response.data.inGameTimestamp;
+          });
       }
     }
   ])
--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,23 @@ var router = express.Router();
 var users = require('./../lib/users');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.send(users.getAll());
 });
 
-router.post('/add', function(req, res, next) {
-  users.add(req.body.name, req.body.region, req.body.username, req.body.password)
-    .then(user => res.status(200).send(user), reason => res.status(400).send(reason))
+router.post('/add', (req, res) => {
+  if(req.body.name && req.body.region) {
+    users.add(req.body.name, req.body.region, req.body.username, req.body.password)
+      .then(user => res.status(200).send(user), reason => res.status(400).send(reason))
+  }
+  else {
+    res.status(400).send('Provide name and region');
+  }
+});
+
+router.get('/:id/currentGame', (req, res) => {
+  users.getCurrentGame(req.params.id)
+    .then(result => res.status(200).send(result));
 })
 
 module.exports = router;
